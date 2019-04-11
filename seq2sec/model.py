861b46395390486929fa08f3574ee908119ab518
Jose Geraldo <zgcarvalho@gmail.com>
2019-04-11 17:55:33 -0300

    class ResNet2 replaces ResNet and ResNetMT. ResNet2 returns a dict with prediction to multiple tasks

--- seq2sec/model.py
+++ seq2sec/model.py
@@ -36,45 +36,83 @@ class ExitLayer(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class ResNet(nn.Module):
-    def __init__(self, n_blocks, chan_out=3, chan_hidden=24):
-        super(ResNet,self).__init__()
-        self.feat = FeatureLayer(chan_in=22, chan_out=chan_hidden)
-        self.block_layers = self._make_block_layers(n_blocks, chan_in=chan_hidden, chan_out=chan_hidden)
-        self.exit = ExitLayer(chan_in=chan_hidden, chan_out=chan_out)
+# class ResNet(nn.Module):
+#     def __init__(self, n_blocks, chan_out=3, chan_hidden=24):
+#         super(ResNet,self).__init__()
+#         self.feat = FeatureLayer(chan_in=22, chan_out=chan_hidden)
+#         self.block_layers = self._make_block_layers(n_blocks, chan_in=chan_hidden, chan_out=chan_hidden)
+#         self.exit = ExitLayer(chan_in=chan_hidden, chan_out=chan_out)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv1d):
-                nn.init.xavier_normal_(m.weight.data, gain=nn.init.calculate_gain('leaky_relu'))
-                # nn.init.kaiming_normal_(m.weight.data, mode='fan_in', nonlinearity='leaky_relu')
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv1d):
+#                 nn.init.xavier_normal_(m.weight.data, gain=nn.init.calculate_gain('leaky_relu'))
+#                 # nn.init.kaiming_normal_(m.weight.data, mode='fan_in', nonlinearity='leaky_relu')
 
     
-    def _make_block_layers(self, n_blocks, chan_in, chan_out):
-        layers = [BlockLayer(chan_in=chan_in, chan_out=chan_out) for i in range(n_blocks)]
-        return nn.Sequential(*layers)
+#     def _make_block_layers(self, n_blocks, chan_in, chan_out):
+#         layers = [BlockLayer(chan_in=chan_in, chan_out=chan_out) for i in range(n_blocks)]
+#         return nn.Sequential(*layers)
         
 
-    def forward(self, x):
-        x = self.feat(x)
-        x = self.block_layers(x)
-        x = self.exit(x)
-        return x
+#     def forward(self, x):
+#         x = self.feat(x)
+#         x = self.block_layers(x)
+#         x = self.exit(x)
+#         return x
 
-    def predict(self, x):
-        x = self.feat(x)
-        results = [nn.functional.softmax(self.exit(block(x)), dim=1).detach().numpy() for block in self.block_layers]
-        return np.stack(results)
+#     def predict(self, x):
+#         x = self.feat(x)
+#         results = [nn.functional.softmax(self.exit(block(x)), dim=1).detach().numpy() for block in self.block_layers]
+#         return np.stack(results)
 
-class ResNetMT(nn.Module):
-    '''chan_out: tuple with channel number per exit layer. Ex (3,4, 8) means three exit layers
-    of a network capable to predict ss3, ss4 and ss8'''
-    def __init__(self, n_blocks, chan_out=(3,), chan_hidden=24):
-        assert(isinstance(chan_out, tuple))
-        super(ResNetMT,self).__init__()
+# class ResNetMT(nn.Module):
+#     '''chan_out: tuple with channel number per exit layer. Ex (3,4, 8) means three exit layers
+#     of a network capable to predict ss3, ss4 and ss8'''
+#     def __init__(self, n_blocks, chan_out=(3,), chan_hidden=24):
+#         assert(isinstance(chan_out, tuple))
+#         super(ResNetMT,self).__init__()
+#         self.feat = FeatureLayer(chan_in=22, chan_out=chan_hidden)
+#         self.block_layers = self._make_block_layers(n_blocks, chan_in=chan_hidden, chan_out=chan_hidden)
+#         # self.exit = ExitLayer(chan_in=chan_hidden, chan_out=chan_out)
+#         self.exit_layers = [ ExitLayer(chan_in=chan_hidden, chan_out=x) for x in chan_out ]
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv1d):
+#                 nn.init.xavier_normal_(m.weight.data, gain=nn.init.calculate_gain('leaky_relu'))
+#                 # nn.init.kaiming_normal_(m.weight.data, mode='fan_in', nonlinearity='leaky_relu')
+
+    
+#     def _make_block_layers(self, n_blocks, chan_in, chan_out):
+#         layers = [BlockLayer(chan_in=chan_in, chan_out=chan_out) for i in range(n_blocks)]
+#         return nn.Sequential(*layers)
+        
+
+#     def forward(self, x):
+#         '''returns a list of tensors where each tensor represents a task'''
+#         x = self.feat(x)
+#         x = self.block_layers(x)
+#         x = [ exitlayer(x) for exitlayer in self.exit_layers ]
+#         return x
+
+#     def predict(self, x):
+#         '''returns a list of lists. The inner list contains the prediction at each resnet block. The
+#         outer list separates the results per tasks'''
+#         x = self.feat(x)
+#         #TODO this comprehension can be optimized. Same blocks are executed n=len(exit_layers) times.
+#         #TODO include a softmax and change to np.array as done in ResNet.predict
+#         results = [[ exitlayer(block(x)) for block in self.block_layers ] for exitlayer in self.exit_layers ]
+#         return results
+
+class ResNet2(nn.Module):
+    '''output: example {'ss_cons_3_label':3}'''
+    def __init__(self, output, n_blocks=21, chan_hidden=24):
+        # assert(isinstance(chan_out, tuple))
+        super(ResNet2,self).__init__()
         self.feat = FeatureLayer(chan_in=22, chan_out=chan_hidden)
         self.block_layers = self._make_block_layers(n_blocks, chan_in=chan_hidden, chan_out=chan_hidden)
         # self.exit = ExitLayer(chan_in=chan_hidden, chan_out=chan_out)
-        self.exit_layers = [ ExitLayer(chan_in=chan_hidden, chan_out=x) for x in chan_out ]
+        # self.exit_layers = [ ExitLayer(chan_in=chan_hidden, chan_out=x) for x in chan_out ]
+        self.exit_layers = {k: ExitLayer(chan_in=chan_hidden, chan_out=output[k]) for k in output}
 
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
@@ -88,20 +126,21 @@ class ResNetMT(nn.Module):
         
 
     def forward(self, x):
-        '''returns a list of tensors where each tensor represents a task'''
+        '''returns a dictionary where keys are task names and values are prediction values'''
         x = self.feat(x)
         x = self.block_layers(x)
-        x = [ exitlayer(x) for exitlayer in self.exit_layers ]
-        return x
+        # x = [ exitlayer(x) for exitlayer in self.exit_layers ]
+        output = {k: self.exit_layers[k](x) for k in self.exit_layers}
+        return output
 
-    def predict(self, x):
-        '''returns a list of lists. The inner list contains the prediction at each resnet block. The
-        outer list separates the results per tasks'''
-        x = self.feat(x)
-        #TODO this comprehension can be optimized. Same blocks are executed n=len(exit_layers) times.
-        #TODO include a softmax and change to np.array as done in ResNet.predict
-        results = [[ exitlayer(block(x)) for block in self.block_layers ] for exitlayer in self.exit_layers ]
-        return results
+    # def predict(self, x):
+    #     '''returns a list of lists. The inner list contains the prediction at each resnet block. The
+    #     outer list separates the results per tasks'''
+    #     x = self.feat(x)
+    #     #TODO this comprehension can be optimized. Same blocks are executed n=len(exit_layers) times.
+    #     #TODO include a softmax and change to np.array as done in ResNet.predict
+    #     results = [[ exitlayer(block(x)) for block in self.block_layers ] for exitlayer in self.exit_layers ]
+    #     return results
 
 def load(filename):
     net = torch.load(filename) 
