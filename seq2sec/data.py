dd9602dac37c77f4f50724f5ef2e45f6237c0d83
Jose Geraldo <zgcarvalho@gmail.com>
2019-04-12 15:45:33 -0300

    remove old and unused code

--- seq2sec/data.py
+++ seq2sec/data.py
@@ -74,29 +74,12 @@ class SSDataset(Dataset):
         ex = {}
         for k in self.examples:
             if k == 'seq_res':
-                # ex[k] = self._encode_aa(self.examples[k][idx])
                 ex[k] = self._pad_input(self.examples[k][idx])
             else:
                 ex[k] = self._pad_label(self.examples[k][idx])
 
         return ex
 
-
-    # def __getitem__(self, idx):
-    #     ex = self.examples[idx]
-    #     # read dataframe
-    #     df = pd.read_feather(self.path + ex['id'] + '.fth')
-    #     # only use the specific chain 
-    #     df = df.loc[df['res_chain'] == ex['chain']]
-
-    #     try: 
-    #         input_data = self._encode_aa(df['seq_res'].values)
-    #         # again...only the first task is used
-    #         output_data = self._pad_label(df[self.tasks[0]].values)
-    #     except ValueError as e:
-    #         print("ID: {} -> Error: {}", ex, e.value)
-
-    #     return (input_data, output_data)
 
     def _read(self, examples_from_context):
         examples = {'seq_res': []}
@@ -116,53 +99,26 @@ class SSDataset(Dataset):
 
         return examples
 
-    # def _encode_aa(self, seq):
-    #     code = torch.zeros((INPUT_CODE, MAX_LENGTH+(2*PAD)), requires_grad=False)
-
-    #     for i in range(PAD):
-    #         code[AA['before'].value, i] = 1
-
-    #     for i, aa in enumerate(seq):
-    #         code[AA[aa].value, i+PAD] = 1
-        
-    #     for i in range(PAD+len(seq), (2*PAD)+len(seq)):
-    #         code[AA['after'].value, i] = 1
-
-    #     # after (pad)+len(seq)+(pad) the values are zero
-
-    #     return code
-
-    def _seq2int(self, seq):
+    @staticmethod
+    def _seq2int(seq):
         code = np.zeros(len(seq), dtype=np.int)
         for i, aa in enumerate(seq):
             code[i] = AA[aa].value
         return code
 
-    def _pad_input(self, x):
+    @staticmethod
+    def _pad_input(x):
         idx = np.pad(x, (PAD,MAX_LENGTH+(2*PAD)-len(x)-PAD), 'constant', constant_values=(AA['before'].value, AA['after'].value))
         m = torch.zeros((INPUT_CODE, MAX_LENGTH+(2*PAD)), requires_grad=False)
         m[idx, np.arange(MAX_LENGTH+(2*PAD))] = 1
         return m
 
-
-    def _pad_label(self, y):
+    @staticmethod
+    def _pad_label(y):
         return torch.from_numpy(np.pad(y, (PAD,MAX_LENGTH+(2*PAD)-len(y)-PAD), 'constant', constant_values=(-1, -1))).type(torch.int64)
 
-        # code = torch.ones((MAX_LENGTH+ (2*PAD)), dtype=torch.int64) * -1 #-1 is the code to ignore
-        
-        # for i, v in enumerate(torch.as_tensor(y)):
-        #     code[i+PAD] = v
-
-        # return code
 
 
-# read len of examples and keep fn and len
-
-# create sets of examples until a max size (these sets will be the new examples)
-
-# len -> number of sets
-
-# getitem -> read files and concatenate
 
 if __name__ == "__main__":
     import time
@@ -190,11 +146,3 @@ if __name__ == "__main__":
     print("Time elapsed: ", time.process_time() - t2)
     
 
-    # d = SSDataset('../data/config/data_cath95.json')
-    # print(d.path)
-    # print(d.tasks)
-    # print(len(d.examples))
-    # for i in range(len(d.examples)):
-    #     a = d[i]
-    #     # print(i)
-    #     # print(a[0].shape, a[1].shape)
