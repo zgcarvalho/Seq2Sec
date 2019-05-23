1274050ecd0edd95971675e27c753ff3a39a4fb6
jgcarvalho <jgcarvalho@pop-os.localdomain>
2019-05-23 07:41:37 -0300

    dropout block out

--- training.py
+++ training.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     # RECEIVED_PARAMS = nni.get_next_parameter()
     # print(RECEIVED_PARAMS)
     # train('data/config/data_test.json', RECEIVED_PARAMS, 'models/teste_mt_3-4.pth')
-    train('data/config/data_test.json','models/teste-uncloss-ss3_ss4_buried.pth', epochs=100)
-    # train('data/config/data_cath95.json','models/resnet_cath95_ss3-ss4-buried.pth', device='cuda', epochs=200)
+    # train('data/config/data_test.json','models/teste-uncloss-ss3_ss4_buried.pth', epochs=100)
+    train('data/config/data_cath95.json','models/resnet_uncertloss_dropout_cath95_ss3-ss4-buried.pth', device='cuda', epochs=200)
     # train('data/config/data_test.json','models/test_hyper_ss3-ss4.pth', device='cuda', epochs=20)
